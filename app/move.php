29558519f925117aa6071a476cda20bdab7fd433
nvm <Never M>
2019-10-18 10:54:38 +0300

    Move common entrypoint code to init.php

--- app/move.php
+++ app/move.php
@@ -1,9 +1,6 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once 'init.php';
 
 $app = new \GeekhubShop\Cli\ConsoleApp();
 $app->moveProduct($argv);
