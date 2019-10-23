211ea3760e61883911d214ea219d810d61a1c37a
nvm <Never M>
2019-10-23 14:29:02 +0300

    Switch to html output in entrypoint

--- public/create.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once 'init.php';
-
-$app = new \GeekhubShop\Cli\ConsoleApp();
-$app->createProduct($argv);
--- public/move.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once 'init.php';
-
-$app = new \GeekhubShop\Cli\ConsoleApp();
-$app->moveProduct($argv);
