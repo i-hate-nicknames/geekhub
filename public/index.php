7800358f7d078f8343a7e4b539a612386035fa8e
nvm <Never M>
2019-10-29 11:33:44 +0200

    Use httpfoundation for request/response

--- public/index.php
+++ public/index.php
@@ -1,8 +1,20 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+$request = Request::createFromGlobals();
+$response = new Response();
+$response->setStatusCode(200);
+$response->setContent('deth');
+$response->headers->set('Content-Type', 'text/html');
+$response->prepare($request);
+$response->send();
+die();
 $app = new \GeekhubShop\App();
 $app->run();
