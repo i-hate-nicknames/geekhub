2127536348ba9b3ca1dde66a7c589d3ac004424a
nvm <Never M>
2019-11-01 15:04:38 +0200

    Move all initialization to DI container

--- public/index.php
+++ public/index.php
@@ -1,16 +1,6 @@
 <?php
 
-use GeekhubShop\Framework\Kernel;
-use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
-use Symfony\Component\HttpKernel\EventListener\ExceptionListener;
-use Symfony\Component\HttpKernel\EventListener\RouterListener;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\Routing\RequestContext;
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
@@ -19,21 +9,11 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $routes = include __DIR__ . '/../src/config.php';
 
+$container = include __DIR__ . '/../src/container.php';
+
 $request = Request::createFromGlobals();
-$requestStack = new RequestStack();
 
-$context = new RequestContext();
-$matcher = new UrlMatcher($routes, $context);
-
-$dispatcher = new EventDispatcher();
-$dispatcher->addSubscriber(new RouterListener($matcher, $requestStack));
-$dispatcher->addSubscriber(new ExceptionListener('GeekhubShop\Controllers\ErrorController::exception'));
-
-$controllerResolver = new ControllerResolver();
-$argumentResolver = new ArgumentResolver();
-
-$kernel = new Kernel($dispatcher, $controllerResolver, $requestStack);
-$response = $kernel->handle($request);
+$response = $container->get('kernel')->handle($request);
 
 $response->prepare($request);
 $response->send();
