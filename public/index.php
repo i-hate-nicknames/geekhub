b5ba805faea733531b5c88f40f615e0cc6c6ccd4
nvm <Never M>
2019-11-01 14:29:38 +0200

    Use HttpKernel class as kernel of the framework

--- public/index.php
+++ public/index.php
@@ -1,10 +1,13 @@
 <?php
 
 use GeekhubShop\Framework\Kernel;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\HttpKernel\EventListener\RouterListener;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
 
@@ -13,25 +16,21 @@ ini_set('display_errors', 1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-function render_template($request)
-{
-    extract($request->attributes->all(), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__ . '/../src/templates/%s.php', $_route);
-    return new Response(ob_get_clean());
-}
-
 $routes = include __DIR__ . '/../src/config.php';
 
 $request = Request::createFromGlobals();
+$requestStack = new RequestStack();
 
 $context = new RequestContext();
 $matcher = new UrlMatcher($routes, $context);
 
+$dispatcher = new EventDispatcher();
+$dispatcher->addSubscriber(new RouterListener($matcher, $requestStack));
+
 $controllerResolver = new ControllerResolver();
 $argumentResolver = new ArgumentResolver();
 
-$kernel = new Kernel($matcher, $controllerResolver, $argumentResolver);
+$kernel = new Kernel($dispatcher, $controllerResolver, $requestStack);
 $response = $kernel->handle($request);
 
 $response->prepare($request);
--- src/Framework/Kernel.php
+++ src/Framework/Kernel.php
@@ -2,55 +2,9 @@
 
 namespace GeekhubShop\Framework;
 
-use Exception;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolverInterface;
-use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
-use Symfony\Component\HttpKernel\HttpKernelInterface;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
-use function call_user_func_array;
+use Symfony\Component\HttpKernel\HttpKernel;
 
-class Kernel implements HttpKernelInterface
+class Kernel extends HttpKernel
 {
-    /** @var UrlMatcherInterface */
-    private $matcher;
 
-    /** @var ControllerResolverInterface */
-    private $controllerResolver;
-
-    /** @var ArgumentResolverInterface */
-    private $argumentResolver;
-
-    /**
-     * Kernel constructor.
-     * @param UrlMatcherInterface $matcher
-     * @param ControllerResolverInterface $controllerResolver
-     * @param ArgumentResolverInterface $argumentResolver
-     */
-    public function __construct(UrlMatcherInterface $matcher, ControllerResolverInterface $controllerResolver, ArgumentResolverInterface $argumentResolver)
-    {
-        $this->matcher = $matcher;
-        $this->controllerResolver = $controllerResolver;
-        $this->argumentResolver = $argumentResolver;
-    }
-
-    public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = true): Response
-    {
-        $this->matcher->getContext()->fromRequest($request);
-
-        try {
-            $request->attributes->add($this->matcher->match($request->getPathInfo()));
-            $controller = $this->controllerResolver->getController($request);
-            $arguments = $this->argumentResolver->getArguments($request, $controller);
-            $response = call_user_func_array($controller, $arguments);
-            $response->headers->set('Content-Type', 'text/html');
-        } catch (ResourceNotFoundException $exception) {
-            $response = new Response('Not Found', 404);
-        } catch (Exception $exception) {
-            $response = new Response('Internal server error: ' . $exception->getMessage(), 500);
-        }
-        return $response;
-    }
 }
