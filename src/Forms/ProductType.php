52621e71d54e7e4694b35c6417dcd0b9d251cb32
nvm <Never M>
2019-12-04 19:33:16 +0200

    Add new form type

---
+++ src/Forms/ProductType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Forms;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('price', NumberType::class)
+            ->add('quantity', NumberType::class)
+            ->add('description', TextareaType::class);
+    }
+}
