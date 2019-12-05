b210322c5b097f82ef5e293caaedcf387832c2b0
nvm <Never M>
2019-12-05 13:25:03 +0200

    Submit form and save product to the db

--- src/Forms/ProductType.php
+++ src/Forms/ProductType.php
@@ -2,11 +2,14 @@
 
 namespace App\Forms;
 
+use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProductType extends AbstractType
 {
@@ -15,7 +18,15 @@ class ProductType extends AbstractType
         $builder
             ->add('name', TextType::class)
             ->add('price', NumberType::class)
-            ->add('quantity', NumberType::class)
-            ->add('description', TextareaType::class);
+            ->add('qty', NumberType::class)
+            ->add('description', TextareaType::class)
+            ->add('save', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class
+        ]);
     }
 }
