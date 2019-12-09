bee0b6f0bfbe43e164e43dd2daaa12b8f0c3de5f
nvm <Never M>
2019-12-09 18:09:47 +0200

    Show product and user listings on frontend

--- src/Controller/StoreController.php
+++ src/Controller/StoreController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\Product;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +16,41 @@ class StoreController extends AbstractController
      * @return Response
      * @throws \Exception
      */
-    public function hello()
+    public function home()
     {
-        return $this->render('categories.html.twig', ['categories' => '']);
+        return $this->redirectToRoute('categories');
+    }
+
+    /**
+     * @Route("/categories", name="categories")
+     * @return Response
+     * @throws \Exception
+     */
+    public function categories()
+    {
+        $repository = $this->getDoctrine()->getRepository(Category::class);
+        return $this->render('categories.html.twig', ['categories' => $repository->findAll()]);
+    }
+
+    /**
+     * @Route("/products", name="products")
+     * @return Response
+     * @throws \Exception
+     */
+    public function products()
+    {
+        $repository = $this->getDoctrine()->getRepository(Product::class);
+        return $this->render('products.html.twig', ['products' => $repository->findAll()]);
+    }
+
+    /**
+     * @Route("/users", name="users")
+     * @return Response
+     * @throws \Exception
+     */
+    public function users()
+    {
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        return $this->render('users.html.twig', ['users' => $repository->findAll()]);
     }
 }
